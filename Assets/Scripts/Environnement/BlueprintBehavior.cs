9d405aedb693d4c01902bc2906652c87efcd1b21
Zaladim <48721924+Zaladim@users.noreply.github.com>
2021-03-31 12:05:12 +0200

    Placement rework : adding room blueprints

---
+++ Assets/Scripts/Environnement/BlueprintBehavior.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueprintBehavior : MonoBehaviour
+{
+    private float tavernLimit = -10;
+    private RaycastHit hit;
+    private GameObject wallAnchor;
+
+    [SerializeField] private GameObject prefab;
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.CompareTag("BorderWall"))
+            {
+                wallAnchor = hit.collider.gameObject;
+                transform.position = hit.collider.transform.position;
+                transform.rotation = hit.collider.transform.rotation;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject o = Instantiate(prefab, transform.position, transform.rotation);
+            gameObject.SetActive(false);
+            
+            foreach (var item in o.GetComponentsInChildren<Transform>())
+            {
+                if (item.CompareTag("BorderWall"))
+                {
+                    if (CheckForOtherRoom(1f, item.gameObject))
+                    {
+                        Destroy(o);
+                        gameObject.SetActive(true);
+                        return;
+                    }
+                }
+            }
+
+            foreach (var item in o.GetComponentsInChildren<Transform>())
+            {
+                if (item.CompareTag("BorderWall"))
+                {
+                    if (item.position.x < tavernLimit)
+                    {
+                        item.gameObject.tag = "Wall";
+                    }
+
+                    DestroyObjectAtLocation(1f, item.gameObject);
+                }
+            }
+            gameObject.SetActive(true);
+            Destroy(wallAnchor);
+            Destroy(gameObject);
+        }
+    }
+
+    private void DestroyObjectAtLocation(float minDist, GameObject item)
+    {
+        Vector3 tmpLocation = item.transform.position;
+        // print(tmpLocation);
+        Transform[] tiles = GameObject.FindObjectsOfType<Transform> ();
+       
+        for (int i = 0; i < tiles.Length; i++) {
+            if(Vector3.Distance(tiles[i].position, tmpLocation) <= minDist){
+                if (item != tiles[i].gameObject)
+                {
+                    item.tag = "Wall";
+                    Destroy(tiles[i].gameObject);
+                }
+            }
+        }
+    }
+
+    private bool CheckForOtherRoom(float minDist, GameObject item)
+    {
+        Vector3 tmpLocation = item.transform.position;
+        // print(tmpLocation);
+        Transform[] tiles = GameObject.FindObjectsOfType<Transform> ();
+        
+        for (int i = 0; i < tiles.Length; i++) {
+            if(Vector3.Distance(tiles[i].position, tmpLocation) <= minDist){
+                if (tiles[i].gameObject.CompareTag("Room"))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/Environnement/PlacementManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementManager : MonoBehaviour
+{
+    [SerializeField] private GameObject roomUI;
+    [SerializeField] private RessourcesManager ressources;
+
+    private int price;
+
+    public int Price
+    {
+        get => price;
+        set => price = value;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (roomUI.gameObject.activeSelf)
+            {
+                roomUI.gameObject.SetActive(false);
+            } else
+            {
+                roomUI.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    public void CreateRoom(GameObject blueprint)
+    {
+        if (ressources.Gold >= price)
+        {
+            Instantiate(blueprint);
+            ressources.Gold -= price;
+            if (roomUI.gameObject.activeSelf)
+            {
+                roomUI.gameObject.SetActive(false);
+            } else
+            {
+                roomUI.gameObject.SetActive(true);
+            }
+        }
+    }
+}
