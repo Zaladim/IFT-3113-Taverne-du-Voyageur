69d445506eb42ae1a9eeacb5a30178632a1d9916
Pierre Marty <pierre.marty.1@ulaval.ca>
2021-04-09 14:54:04 -0400

    fix Client's resourceManager access

--- Assets/Scripts/Characters/Waiter.cs
+++ Assets/Scripts/Characters/Waiter.cs
@@ -4,45 +4,38 @@ using UnityEngine;
 
 namespace Characters
 {
+    public enum WaiterState
+    {
+        Neutral,
+        TakingClientOrder,
+        GoToCounter,
+        GivingOrder,
+        Idle //wait a little bit before leaving client to go back to counter
+    }
+
     public class Waiter : MonoBehaviour
     {
-        public enum WaiterState
-        {
-            Neutral,
-            TakingClientOrder,
-            GoToCounter,
-            GivingOrder,
-            Idle //wait a little bit before leaving client to go back to counter
-        }
-
-        private BasicAI mouvement;
-
-        private TextMesh text;
-
-        private TextMesh subText;
-
-        private List<Client> clients;
-
-        private Client currentClient;
-
-        private GameObject foodLocation;
-
-        private GameObject lookDirection;
-
-        private WaiterState etat;
-
-        public float distanceFromClientToInteract = 2;
-
-        private float idleTimer;
-
-        public float timeToStayIdleMin = 1f;
-        public float timeToStayIdleMax = 10f;
+        [Header("Options")]
+        [SerializeField] private float distanceFromClientToInteract = 2;
+        [SerializeField] private float timeToStayIdleMin = 1f;
+        [SerializeField] private float timeToStayIdleMax = 10f;
+        
+        [Header("Debug")]
+        [SerializeField] private float idleTimer;
+        [SerializeField] private List<Client> clients;
+        [SerializeField] private Client currentClient;
+        [SerializeField] private WaiterState etat;
+        [SerializeField] private GameObject foodLocation;
+        [SerializeField] private GameObject lookDirection;
+        [SerializeField] private BasicAI mouvement;
+        [SerializeField] private TextMesh subText;
+        [SerializeField] private TextMesh text;
 
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
             mouvement = GetComponent<BasicAI>();
-            Transform temp = GetComponent<Transform>().Find("Text");
+            var temp = GetComponent<Transform>().Find("Text");
             text = temp.GetComponent<TextMesh>();
             temp = GetComponent<Transform>().Find("SubText");
             subText = temp.GetComponent<TextMesh>();
@@ -52,110 +45,102 @@ namespace Characters
         }
 
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
-            if (etat == WaiterState.Neutral)
+            switch (etat)
             {
-                if (lookDirection != null)
-                {
-                    mouvement.FaceLocation(lookDirection.transform.position);
-                }
+                case WaiterState.Neutral:
+                    if (lookDirection != null) mouvement.FaceLocation(lookDirection.transform.position);
 
-                //check if any of its own orders are ready
-                for (int i = 0; i < clients.Count; i++)
-                {
-                    if (clients[i].TimeLeft <= 0 &&
-                        clients[i].GETState() == ClientState.WaitingToReciveOrder)
-                    {
-                        currentClient = clients[i];
-                        mouvement.GoToSpecificClient(currentClient);
-                        etat = WaiterState.GivingOrder;
-                    }
-                }
+                    //check if any of its own orders are ready
+                    for (var i = 0; i < clients.Count; i++)
+                        if (clients[i].TimeLeft <= 10 &&
+                            clients[i].GETState() == ClientState.WaitingToReciveOrder)
+                        {
+                            currentClient = clients[i];
+                            mouvement.GoToSpecificClient(currentClient);
+                            etat = WaiterState.GivingOrder;
+                        }
 
-                //look for a new client
-                if (currentClient == null)
-                {
-                    text.text = "Finding Client";
-                    Client person = mouvement.GoToRandomClient();
-                    if (person == null)
+                    //look for a new client
+                    if (currentClient == null)
                     {
-                        subText.text = "Client Not Found";
+                        text.text = "Finding Client";
+                        var person = mouvement.GoToRandomClient();
+                        if (person == null)
+                        {
+                            subText.text = "Client Not Found";
+                        }
+                        else
+                        {
+                            subText.text = "Client Found";
+                            currentClient = person;
+                            clients.Add(person);
+                            etat = WaiterState.TakingClientOrder;
+                        }
                     }
-                    else
+
+                    break;
+                case WaiterState.TakingClientOrder:
+                    text.text = "Taking Client's Order";
+                    subText.text = "";
+                    if (mouvement.IsCloseToLocation(currentClient.GETPosition(), distanceFromClientToInteract))
                     {
-                        subText.text = "Client Found";
-                        currentClient = person;
-                        clients.Add(person);
-                        etat = WaiterState.TakingClientOrder;
+                        currentClient.InteractWIthClient();
+                        etat = WaiterState.Idle;
+                        idleTimer = Random.Range(timeToStayIdleMin, timeToStayIdleMax);
                     }
-                }
-            }
-            else if (etat == WaiterState.TakingClientOrder)
-            {
-                text.text = "Taking Client's Order";
-                subText.text = "";
-                if (mouvement.IsCloseToLocation(currentClient.GETPosition(), distanceFromClientToInteract))
-                {
-                    currentClient.InteractWIthClient();
-                    etat = WaiterState.Idle;
-                    idleTimer = Random.Range(timeToStayIdleMin, timeToStayIdleMax);
-                }
-            }
-            else if (etat == WaiterState.GoToCounter)
-            {
-                text.text = "Going To Counter";
-                if (currentClient != null)
-                {
-                    subText.text = "Counter Not Found";
-                    currentClient = null;
-                    foodLocation = mouvement.GoToCounter(out lookDirection);
-                }
-                else
-                {
-                    if (foodLocation == null)
+
+                    break;
+                case WaiterState.GoToCounter:
+                    text.text = "Going To Counter";
+                    if (currentClient != null)
                     {
                         subText.text = "Counter Not Found";
+                        currentClient = null;
                         foodLocation = mouvement.GoToCounter(out lookDirection);
                     }
                     else
                     {
-                        subText.text = "Counter Found";
-                        if (mouvement.IsAtLocation(foodLocation.transform.position))
+                        if (foodLocation == null)
                         {
-                            if (lookDirection == null)
+                            subText.text = "Counter Not Found";
+                            foodLocation = mouvement.GoToCounter(out lookDirection);
+                        }
+                        else
+                        {
+                            subText.text = "Counter Found";
+                            if (mouvement.IsAtLocation(foodLocation.transform.position))
                             {
-                                transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-                            }
+                                if (lookDirection == null) transform.rotation = Quaternion.Euler(0f, 0f, 0f);
 
-                            etat = WaiterState.Neutral;
-                            foodLocation = null;
+                                etat = WaiterState.Neutral;
+                                foodLocation = null;
+                            }
                         }
                     }
-                }
-            }
-            else if (etat == WaiterState.GivingOrder)
-            {
-                text.text = "Giving Client's Order";
-                subText.text = "";
-                if (mouvement.IsCloseToLocation(currentClient.GETPosition(), distanceFromClientToInteract))
-                {
-                    currentClient.InteractWIthClient();
-                    etat = WaiterState.Idle;
-                    idleTimer = Random.Range(timeToStayIdleMin, timeToStayIdleMax);
-                    clients.Remove(currentClient);
-                }
-            }
-            else if (etat == WaiterState.Idle)
-            {
-                mouvement.StopMoving();
-                mouvement.FaceLocation(currentClient.gameObject.transform.position);
-                subText.text = "Time Left: " + Mathf.Ceil(idleTimer) + "s";
-                idleTimer -= Time.deltaTime;
-                if (idleTimer <= 0)
-                {
-                    etat = WaiterState.GoToCounter;
-                }
+
+                    break;
+                case WaiterState.GivingOrder:
+                    text.text = "Giving Client's Order";
+                    subText.text = "";
+                    if (mouvement.IsCloseToLocation(currentClient.GETPosition(), distanceFromClientToInteract))
+                    {
+                        currentClient.InteractWIthClient();
+                        etat = WaiterState.Idle;
+                        idleTimer = Random.Range(timeToStayIdleMin, timeToStayIdleMax);
+                        clients.Remove(currentClient);
+                    }
+
+                    break;
+                case WaiterState.Idle:
+                    mouvement.StopMoving();
+                    mouvement.FaceLocation(currentClient.gameObject.transform.position);
+                    subText.text = "Time Left: " + Mathf.Ceil(idleTimer) + "s";
+                    idleTimer -= Time.deltaTime;
+                    if (idleTimer <= 0) etat = WaiterState.GoToCounter;
+
+                    break;
             }
         }
     }
