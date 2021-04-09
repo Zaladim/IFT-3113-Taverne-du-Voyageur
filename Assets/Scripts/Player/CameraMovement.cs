8909f99701674a767af3abe79651775fe1aa7238
Pierre Marty <pierre.marty.1@ulaval.ca>
2021-04-09 12:13:28 -0400

    fix nodes height

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -12,45 +12,34 @@ namespace Player
         [SerializeField] private float scrollSpeed = 20f;
         [SerializeField] private float minY = 15f;
         [SerializeField] private float maxY = 50f;
-    
+
+        [SerializeField] private bool mouseControl = false;
+
         private void Update()
         {
-            Vector3 pos = transform.position;
+            var pos = transform.position;
 
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-            float scroll = Input.GetAxis("Mouse ScrollWheel");
-        
-            if (Input.mousePosition.y >= Screen.height - panBorderThickness)
+            var horizontalInput = Input.GetAxis("Horizontal");
+            var verticalInput = Input.GetAxis("Vertical");
+            var scroll = Input.GetAxis("Mouse ScrollWheel");
+
+            if (mouseControl)
             {
-                verticalInput = 1;
-            }
-        
-            if (Input.mousePosition.y <= panBorderThickness)
-            {
-                verticalInput = -1;
-            }
-        
-            if (Input.mousePosition.x >= Screen.width - panBorderThickness)
-            {
-                horizontalInput = 1;
-            }
-        
-            if (Input.mousePosition.x <= panBorderThickness)
-            {
-                horizontalInput = -1;
+                if (Input.mousePosition.y >= Screen.height - panBorderThickness) verticalInput = 1;
+                if (Input.mousePosition.y <= panBorderThickness) verticalInput = -1;
+                if (Input.mousePosition.x >= Screen.width - panBorderThickness) horizontalInput = 1;
+                if (Input.mousePosition.x <= panBorderThickness) horizontalInput = -1;
             }
 
             pos.x += horizontalInput * panSpeed * Time.deltaTime;
             pos.z += verticalInput * panSpeed * Time.deltaTime;
             pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
-            
+
             pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
             pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
             pos.y = Mathf.Clamp(pos.y, minY, maxY);
-        
+
             transform.position = pos;
-        
         }
     }
 }
