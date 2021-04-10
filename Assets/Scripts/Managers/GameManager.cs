e46b6690010eace0e33cd47685a6eca4120a8553
Pierre Marty <pierre.marty.1@ulaval.ca>
2021-04-09 22:40:29 -0400

    feat: StartMenu

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,7 +4,38 @@ namespace Managers
 {
     public class GameManager : MonoBehaviour
     {
+        [Header("Managers")] [SerializeField] private ClientManager clientManager;
+        [SerializeField] private WaiterManager waiterManager;
+        [SerializeField] private PlacementManager placementManager;
+        [SerializeField] private ResourcesManager resourcesManager;
+
+        [Header("Game Elements")] [SerializeField]
+        private GameObject startPanel;
+
+        [SerializeField] private GameObject gamePanel;
         public bool MouseControl { get; set; }
+
+        private void Start()
+        {
+            startPanel.SetActive(true);
+            gamePanel.SetActive(false);
+
+            clientManager.gameObject.SetActive(false);
+            waiterManager.gameObject.SetActive(false);
+            placementManager.gameObject.SetActive(false);
+            resourcesManager.gameObject.SetActive(false);
+        }
+
+        public void StartGame()
+        {
+            startPanel.SetActive(false);
+            gamePanel.SetActive(true);
+
+            clientManager.gameObject.SetActive(true);
+            waiterManager.gameObject.SetActive(true);
+            placementManager.gameObject.SetActive(true);
+            resourcesManager.gameObject.SetActive(true);
+        }
 
         public void QuitGame()
         {
