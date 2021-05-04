a8cbc6698537930c53da6eaee50cf998e7fa3ad5
Alex Bouchard <alexbouchard04@gmail.com>
2021-05-04 19:25:11 -0400

    corriger une erreur lorsqu'aucun siège n'est libre

--- Assets/Scripts/Characters/Client.cs
+++ Assets/Scripts/Characters/Client.cs
@@ -105,7 +105,10 @@ namespace Characters
                             {
                                 newDestinationTimer = -1;
                                 seat = mouvement.GoToRandomSeat();
-                                lookDirection = seat.lookDirection;
+                                if (seat != null)
+                                {
+                                    lookDirection = seat.lookDirection;
+                                }
                             }
                         }
                         else
