2f60bf12da5aa226aa6ff3466818135c7a8bd2b7
Pierre Marty <pierre.marty.1@ulaval.ca>
2021-05-06 17:10:21 -0400

    feat: Add notification

---
+++ Assets/Scripts/Interface/Notification.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Interface
+{
+    public class Notification : MonoBehaviour
+    {
+        [SerializeField] private Text textZone;
+        [SerializeField] private Text timeZone;
+        public float duration = 3f;
+
+        public void Update()
+        {
+            duration -= Time.unscaledDeltaTime;
+            timeZone.text = ((int) duration).ToString(CultureInfo.InvariantCulture);
+
+            if (duration <= 0) Destroy(gameObject);
+        }
+
+        public void SetMessage(string msg)
+        {
+            textZone.text = msg;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/NotificationSystem.cs
@@ -0,0 +1,20 @@
+﻿using Interface;
+using UnityEngine;
+
+namespace Managers
+{
+    public class NotificationSystem : MonoBehaviour
+    {
+        [SerializeField] private Transform notificationAreaCenter;
+        [SerializeField] private Notification notificationWidget;
+
+        public void CreateNotification(string msg, float duration = 3)
+        {
+            if (!gameObject.activeSelf) return;
+
+            var notification = Instantiate(notificationWidget, notificationAreaCenter);
+            notification.SetMessage(msg);
+            notification.duration = duration;
+        }
+    }
+}
