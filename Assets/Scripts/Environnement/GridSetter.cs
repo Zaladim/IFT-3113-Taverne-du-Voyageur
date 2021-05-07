dae90607d8ef33bd90e6b253ebfa1e7a89807fda
Zaladim <48721924+Zaladim@users.noreply.github.com>
2021-05-07 14:51:25 +0200

    Corrected wall rotation

--- Assets/Scripts/Environnement/GridSetter.cs
+++ Assets/Scripts/Environnement/GridSetter.cs
@@ -27,13 +27,13 @@ namespace Environnement
             grid.SetValue(transform.position, -1);
             int x, y;
             grid.getXY(transform.position, out x, out y);
+            int rotation = NormalizedRotation();
 
             if (objectWidth > 0)
             {
                 for (int i = 1; i <= objectWidth; i++)
                 {
-                    if ((int) transform.rotation.eulerAngles.y == 0 || (int) transform.rotation.eulerAngles.y == 180 ||
-                        (int) transform.rotation.eulerAngles.y == -180)
+                    if (rotation == 0 || rotation == 180 || rotation == -180)
                     {
                         if (pivot)
                         {
@@ -64,7 +64,7 @@ namespace Environnement
 
             for (int i = 1; i <= objectLength; i++)
             {
-                if ((int)transform.rotation.eulerAngles.y == 0 || (int)transform.rotation.eulerAngles.y == 180 || (int)transform.rotation.eulerAngles.y == -180)
+                if (rotation == 0 || rotation == 180 || rotation == -180)
                 {
                     if (pivot)
                     {
@@ -93,7 +93,7 @@ namespace Environnement
                 
                 for (int j = 1; j <= objectWidth; j++)
                 {
-                    if ((int)transform.rotation.eulerAngles.y == 0 || (int)transform.rotation.eulerAngles.y == 180 || (int)transform.rotation.eulerAngles.y == -180)
+                    if (rotation == 0 || rotation == 180 || rotation == -180)
                     {
                         if (pivot)
                         {
@@ -129,6 +129,43 @@ namespace Environnement
                     }
                 }
             }
+        }
+
+        private int NormalizedRotation()
+        {
+            int rotation = (int)transform.rotation.eulerAngles.y;
+            if (rotation % 10 != 0)
+            {
+                rotation += 1;
+            }
+            if (rotation % 10 != 0)
+            {
+                rotation -= 2;
+            }
+
+            return rotation;
+
+            // if (rotation > -45 && rotation < 45)
+            // {
+            //     rotation = 0;
+            // }
+            //
+            // if (rotation > 45 && rotation < 135)
+            // {
+            //     rotation = 90;
+            // }
+            //
+            // if (rotation > 135 && rotation < 225)
+            // {
+            //     rotation = 180;
+            // }
+            //
+            // if (rotation > 225 && rotation < 315)
+            // {
+            //     rotation = 270;
+            // }
+            //
+            // if (rotation < -45 && rotation )
         }
     }
 }
